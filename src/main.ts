03439e654d7d3d651a97b515fe3b3184e4ffb213
CodiProgs <samuil.codiWork@mail.ru>
2024-03-21 00:42:48 +0900

    upd for deploy port

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,6 @@ async function bootstrap() {
     })
   )
   app.useGlobalFilters(new GraphQLErrorFilter());
-  await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
